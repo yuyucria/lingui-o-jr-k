bea973dee349360c1d8cb4746494c76bc37fc112
vini o gotoso <vinicius.oliveira.peres@escola.pr.gov.br>
2026-05-08 11:37:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
